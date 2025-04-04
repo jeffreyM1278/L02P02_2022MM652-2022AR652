3aa97a92f0d1d8e4703a1fd56d193737c8578a07
jeffr <jeffr@LAPTOP-NS6Q6ARS>
2025-04-03 21:18:29 -0600

    cambios de arreglos

--- L02P02_2022MM652-2022AR652/Models/Libro.cs
+++ L02P02_2022MM652-2022AR652/Models/Libro.cs
@@ -1,6 +1,6 @@
 ﻿namespace L02P02_2022MM652_2022AR652.Models
 {
-    public class Libros
+    public class Libro
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
