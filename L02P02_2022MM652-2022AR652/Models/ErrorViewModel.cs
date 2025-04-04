3c03da696e4398cb9f36105717bc3843f69c19d7
jeffr <jeffr@LAPTOP-NS6Q6ARS>
2025-04-03 18:56:24 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022MM652-2022AR652/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022MM652_2022AR652.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
