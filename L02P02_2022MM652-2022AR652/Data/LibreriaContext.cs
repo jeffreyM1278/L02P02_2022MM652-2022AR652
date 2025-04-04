c3069724c06509d49ac85a322107b1891d39b670
MiguelRamos-145 <miguel.arevalo@catolica.edu.sv>
2025-04-03 20:08:23 -0600

    libreria context

---
+++ L02P02_2022MM652-2022AR652/Data/LibreriaContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2022MM652_2022AR652.Data
+{
+    public class LibreriaContext: DbContext
+    {
+        public LibreriaContext(DbContextOptions<LibreriaContext> options) : base(options) { }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<ComentarioLibro> ComentariosLibros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Libro>()
+                .HasOne(a => a.Autor)
+                .WithMany(l => l.Libros)
+                .HasForeignKey(l => l.IdAutor);
+        }
+    }
+
+}
--- L02P02_2022MM652-2022AR652/Models/Miguel es hora de cambiar la laptop.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace L02P02_2022MM652_2022AR652.Models
-{
-    public class Miguel_es_hora_de_cambiar_la_laptop
-    {
-    }
-}
